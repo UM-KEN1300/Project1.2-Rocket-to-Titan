d5bd309df278c16e196885236122f1d41a1abc88
egedemirelli <104143758+egedemirelli@users.noreply.github.com>
2023-03-20 14:23:33 +0100

    Flipped the variables and completed the solar system

--- src/main/java/group1/project12group1/SolarSystem.java
+++ src/main/java/group1/project12group1/SolarSystem.java
@@ -5,14 +5,20 @@ public class SolarSystem{
 
     public static void main(String[] args)
     {
-        //todo fix the variables they were flipped
 
-        PlanetObject Sun = new PlanetObject(0, 0, 0, 1.99e30, 0, 0, 0);
-        PlanetObject Mercury = new PlanetObject(7.83e6, 4.49e7, 2.87e6, 3.3e23, -5.75e1, 1.15e1, 6.22e0);
-        PlanetObject Venus = new PlanetObject(-2.82e7, 1.04e8, 3.01e6, 4.87e24, -3.4e1, -8.97e0, 1.84e0);
-        PlanetObject Earth = new PlanetObject(-1.48e8, -2.78e7, 3.37e4, 5.97e24, 5.05e0, -2.94e1, 1.71e-3);
+        PlanetObject Sun = new PlanetObject(0, 0, 0, 0, 0, 0, 1.99e30);
+        PlanetObject Mercury = new PlanetObject(7.83e6, 4.49e7, 2.87e6, 5.75e1, 1.15e1, 6.22e0, 3.30e23);
+        PlanetObject Venus = new PlanetObject(-2.82e7, 1.04e8, 3.01e6, -3.4e1, -8.97e0, 1.84e0, 4.87e24);
+        PlanetObject Earth = new PlanetObject(-1.48e8, -2.78e7, 3.37e4, 5.05e0, -2.94e1,1.71e-3 , 5.97e24);
         PlanetObject Moon = new PlanetObject(-1.48e8, -2.75e7, 7.02e4, 4.34e0, -3.0e1, -1.16e2, 7.35e22);
         PlanetObject Mars = new PlanetObject(-1.59e8, 1.89e8, 7.87e6, -1.77e1, -1.35e1, 1.52e1, 6.42e23);
+        PlanetObject Jupiter = new PlanetObject(6.93e8, 2.59e8, -1.66e7, -4.71e0, 1.29e1, 5.22e-2, 1.90e27);
+        PlanetObject Saturn = new PlanetObject(1.25e9, -7.60e8, -3.67e7, 4.47e0, 8.24e0, -3.21e1, 5.68e26);
+        PlanetObject Titan = new PlanetObject(1.25e9, -7.61e8, -3.63e7, 9, 1.11e1, -2.25e0, 1.35e23);
+        PlanetObject Neptune = new PlanetObject(4.45e9, -3.98e8, -9.45e7, 4.48e-1,5.45e0, -1.23e1, 1.02e26);
+        PlanetObject Uranus = new PlanetObject(1.96e9, 2.19e9, -1.72e7, -5.13e0, 4.22e0, 8.21e-2, 8.68e25);
+
+
 
 
         PlanetObject testEarth=new PlanetObject(0,0,0,5.97e24);
