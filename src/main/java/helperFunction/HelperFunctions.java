b2c2d8ef36a122724916af2f03d4f07d8fcd2dfa
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-05-05 06:45:18 +0200

    some JUnit testing for helper functions

--- src/main/java/helperFunction/HelperFunctions.java
+++ src/main/java/helperFunction/HelperFunctions.java
@@ -1,79 +1,106 @@
 package helperFunction;
+
 import Model.PlanetObject;
 
+/**
+ * HelperFunctions class contains utility methods for operations
+ * related to distance calculation, vector addition, subtraction,
+ * and string-to-vector conversion.
+ */
+public class HelperFunctions {
 
-public class HelperFunctions
-{
+    public HelperFunctions() {
+    }
 
-    public HelperFunctions()
-    {}
-
-
-    public static double getDistanceBetween(PlanetObject one, PlanetObject two)
-    {
-        return (Math.sqrt(
-                Math.pow((two.getPositionalVector()[0]-one.getPositionalVector()[0]),2)
-                +Math.pow((two.getPositionalVector()[1]-one.getPositionalVector()[1]),2)
-                +Math.pow((two.getPositionalVector()[2]-one.getPositionalVector()[2]),2))
+    /**
+     * Calculate the distance between two PlanetObjects.
+     *
+     * @param one The first PlanetObject.
+     * @param two The second PlanetObject.
+     * @return The distance between the two PlanetObjects.
+     */
+    public static double getDistanceBetween(PlanetObject one, PlanetObject two) {
+        return Math.sqrt(
+                Math.pow((two.getPositionalVector()[0] - one.getPositionalVector()[0]), 2)
+                        + Math.pow((two.getPositionalVector()[1] - one.getPositionalVector()[1]), 2)
+                        + Math.pow((two.getPositionalVector()[2] - one.getPositionalVector()[2]), 2)
         );
     }
 
-    public static double getDistanceBetweenWithVectors(double[] one, double[] two)
-    {
-        return (Math.sqrt(
-                Math.pow((two[0]-one[0]),2)
-                        +Math.pow((two[1]-one[1]),2)
-                        +Math.pow((two[2]-one[2]),2))
+    /**
+     * Calculate the distance between two positional vectors.
+     *
+     * @param one The first positional vector.
+     * @param two The second positional vector.
+     * @return The distance between the two vectors.
+     */
+    public static double getDistanceBetweenWithVectors(double[] one, double[] two) {
+        return Math.sqrt(
+                Math.pow((two[0] - one[0]), 2)
+                        + Math.pow((two[1] - one[1]), 2)
+                        + Math.pow((two[2] - one[2]), 2)
         );
     }
 
-
-    public static double[] subtract(double[]vectorOne, double[] vectorTwo)
-    {
-       double[] returnable=new double[3];
-       returnable[0]=vectorOne[0]-vectorTwo[0];
-        returnable[1]=vectorOne[1]-vectorTwo[1];
-        returnable[2]=vectorOne[2]-vectorTwo[2];
+    /**
+     * Subtract two vectors.
+     *
+     * @param vectorOne The first vector.
+     * @param vectorTwo The second vector.
+     * @return The resulting vector after subtraction.
+     */
+    public static double[] subtract(double[] vectorOne, double[] vectorTwo) {
+        double[] returnable = new double[3];
+        returnable[0] = vectorOne[0] - vectorTwo[0];
+        returnable[1] = vectorOne[1] - vectorTwo[1];
+        returnable[2] = vectorOne[2] - vectorTwo[2];
         return returnable;
     }
 
-    public static double[] addition(double[]vectorOne, double[] vectorTwo)
-    {
-        double[] returnable=new double[3];
-        returnable[0]=vectorOne[0]+vectorTwo[0];
-        returnable[1]=vectorOne[1]+vectorTwo[1];
-        returnable[2]=vectorOne[2]+vectorTwo[2];
+    /**
+     * Add two vectors.
+     *
+     * @param vectorOne The first vector.
+     * @param vectorTwo The second vector.
+     * @return The resulting vector after addition.
+     */
+    public static double[] addition(double[] vectorOne, double[] vectorTwo) {
+        double[] returnable = new double[3];
+        returnable[0] = vectorOne[0] + vectorTwo[0];
+        returnable[1] = vectorOne[1] + vectorTwo[1];
+        returnable[2] = vectorOne[2] + vectorTwo[2];
         return returnable;
     }
 
-    public double getVectorMagnitude(double[] vector)
-    {
-        return Math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]+vector[2]*vector[2]);
+    /**
+     * Calculate the magnitude of a vector.
+     *
+     * @param vector The input vector.
+     * @return The magnitude of the vector.
+     */
+    public double getVectorMagnitude(double[] vector) {
+        return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
     }
 
-    public static double[] stringToVector(String input)
-    {
-        input = (input.replace("X",""));
-        input = (input.replace("V",""));
-        input = (input.replace("Y",""));
-        input = (input.replace("Z",""));
-        input = (input.replace("=",""));
+    /**
+     * Convert a string representation of a vector to a double array.
+     *
+     * @param input The input string.
+     * @return The vector as a double array.
+     */
+    public static double[] stringToVector(String input) {
+        input = input.replaceAll("[XVYZ=]", "");
+        String[] str = input.trim().split("\\s+");
 
-        String[] srt = input.split(" ");
-        double[] returnable= new double[3];
-        int counter=0;
-        for (int i = 0; i < srt.length; i++)
-        {
-            if (!srt[i].isEmpty())
-            {
-                returnable[counter]=Double.valueOf(srt[i]);
+        double[] returnable = new double[3];
+        int counter = 0;
+        for (String s : str) {
+            if (!s.isEmpty()) {
+                returnable[counter] = Double.parseDouble(s);
                 counter++;
             }
-
         }
-
 
         return returnable;
     }
-
 }
---
+++ src/test/HelperFunctionsTest.java
@@ -0,0 +1,51 @@
+import helperFunction.HelperFunctions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests methods in helperFunction.HelperFunctions
+ */
+class HelperFunctionsTest {
+    @Test
+    void testGetDistanceBetweenWithVectors() {
+        double[] one = new double[]{0, 0, 0};
+        double[] two = new double[]{3, 4, 0};
+
+        double distance = HelperFunctions.getDistanceBetweenWithVectors(one, two);
+        assertEquals(5, distance, "The distance between the two vectors should be 5");
+    }
+
+    @Test
+    void testGetDistanceBetweenWithVectors_EdgeCase() {
+        double[] one = new double[]{Double.MAX_VALUE, 0, 0};
+        double[] two = new double[]{-Double.MAX_VALUE, 0, 0};
+
+        double distance = HelperFunctions.getDistanceBetweenWithVectors(one, two);
+        assertEquals(Double.POSITIVE_INFINITY, distance, "The distance between the two vectors should be 2 * Double.MAX_VALUE");
+    }
+
+    @Test
+    void testAddition() {
+        double[] vectorOne = new double[]{5, 7, 9};
+        double[] vectorTwo = new double[]{2, 3, 4};
+
+        double[] result = HelperFunctions.addition(vectorOne, vectorTwo);
+        assertArrayEquals(new double[]{7, 10, 13}, result, "The resulting vector after addition should be {7, 10, 13}");
+    }
+
+    @Test
+    void testStringToVector() {
+        String input = "X = 1.0 Y = 2.0 Z = 3.0";
+        double[] result = HelperFunctions.stringToVector(input);
+        assertArrayEquals(new double[]{1, 2, 3}, result, "The resulting vector should be {1, 2, 3}");
+    }
+
+    @Test
+    void testStringToVector_EdgeCase() {
+        String input = "X = " + Double.MAX_VALUE + " Y = " + (-Double.MAX_VALUE) + " Z = 0";
+        double[] result = HelperFunctions.stringToVector(input);
+        assertArrayEquals(new double[]{Double.MAX_VALUE, -Double.MAX_VALUE, 0}, result, "The resulting vector should be {Double.MAX_VALUE, -Double.MAX_VALUE, 0}");
+    }
+}
