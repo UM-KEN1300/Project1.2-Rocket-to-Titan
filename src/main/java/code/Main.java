c30e704d34a00e7748c35990d31244a0c561e780
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-05-23 12:42:08 +0200

    .

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,11 +1,15 @@
 package code;
 
 
+import code.algorithms.ModelRunner;
 import code.graphics.Visualizer;
 import code.model.Model;
 import code.model.data.loaders.FileDataLoader;
 import code.model.data.loaders.NasaDataLoader;
+import code.model.objects.PlanetObject;
 import code.model.objects.Probe;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +17,6 @@ public class Main {
         Model.addProbe(new Probe(new double[]{68.7449, -44.1, -2.2176}));
 
         Visualizer.main(args);
+        ModelRunner.runnerForMultipleProbes(366, 1, new ArrayList<>(Model.getPlanetObjects().values()), Model.getProbes());
     }
 }
