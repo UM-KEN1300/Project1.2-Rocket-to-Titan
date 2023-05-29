38d0a0b56a0ea61057ab0ef3036ab15c1ab6f1c2
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-05-29 11:39:35 +0200

    idk at this point

--- src/main/java/code/algorithms/ModelRunner.java
+++ src/main/java/code/algorithms/ModelRunner.java
@@ -1,9 +1,7 @@
 package code.algorithms;
 
-import code.model.Model;
 import code.model.objects.PlanetObject;
 import code.model.objects.Probe;
-import code.utils.HelperFunctions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +10,18 @@ public class ModelRunner {
     private ModelRunner() {
     }
 
+
     public static void runnerForGUI(int smoothness, double accuracy, PlanetObject[] planets) {
         for (int i = 0; i < smoothness; i += 1) {
             for (int j = 1; j < planets.length; j++) {
                 planets[j].initializeAcceleration();
                 for (int k = 0; k < planets.length; k++) {
                     if (k != j) {
-                        Solvers.explicitEuler(planets[j], planets[k], accuracy);
+                        //Solvers.explicitEuler(planets[j], planets[k], accuracy);
+                        Solvers.rungeKutta4(planets[j], planets[k], accuracy);
+                        //Solvers.ralston2(planets[j], planets[k], accuracy*0.625);
+                        //Solvers.kutta3(planets[j], planets[k], accuracy);
+                        //Solvers.heun3(planets[j], planets[k], accuracy*0.625);
                     }
                 }
             }
@@ -42,11 +45,16 @@ public class ModelRunner {
 
     public static void runnerForModel(int numberOfDays, double accuracy, PlanetObject[] planets) {
         for (int i = 0; i < (1 / accuracy) * 60 * 60 * 24 * numberOfDays; i += 1) {
+            if (i % ((1 / accuracy) * 60 * 60 * 24) == 0) {
+                System.out.println(i / ((1 / accuracy) * 60 * 60 * 24));
+            }
             for (int j = 1; j < planets.length; j++) {
                 double[] acc = new double[3];
+                planets[j].initializeAcceleration();
                 for (int k = 0; k < planets.length; k++) {
                     if (k != j) {
-                        acc = HelperFunctions.addition(acc, planets[j].accelerationBetween(planets[k]));
+                        //acc = HelperFunctions.addition(acc, planets[j].accelerationBetween(planets[k]));
+                        Solvers.explicitEuler(planets[j], planets[k], accuracy);
                     }
                 }
 //                Solvers.eulerSolver(planets[j], acc, accuracy);
