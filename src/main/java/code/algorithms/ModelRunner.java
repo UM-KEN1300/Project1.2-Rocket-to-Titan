671dbfa03efb590d93468f5ec1f7707c222e370b
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-05-29 12:08:10 +0200

    .

--- src/main/java/code/algorithms/ModelRunner.java
+++ src/main/java/code/algorithms/ModelRunner.java
@@ -1,6 +1,5 @@
 package code.algorithms;
 
-import code.model.Model;
 import code.model.objects.PlanetObject;
 import code.model.objects.Probe;
 import code.utils.HelperFunctions;
@@ -12,14 +11,14 @@ public class ModelRunner {
     private ModelRunner() {
     }
 
+
     public static void runnerForGUI(int smoothness, double accuracy, PlanetObject[] planets) {
         for (int i = 0; i < smoothness; i += 1) {
             for (int j = 1; j < planets.length; j++) {
                 planets[j].initializeAcceleration();
                 for (int k = 0; k < planets.length; k++) {
-                    if (k != j) {
+                    if (k != j)
                         Solvers.explicitEuler(planets[j], planets[k], accuracy);
-                    }
                 }
             }
         }
@@ -49,9 +48,8 @@ public class ModelRunner {
 
     public static void runnerForModel(int numberOfDays, double accuracy, PlanetObject[] planets) {
         for (int i = 0; i < (1 / accuracy) * 60 * 60 * 24 * numberOfDays; i += 1) {
-            if(i%((1 / accuracy) * 60 * 60 * 24)==0)
-            {
-                System.out.println(i /( (1 / accuracy) * 60 * 60 * 24));
+            if (i % ((1 / accuracy) * 60 * 60 * 24) == 0) {
+                System.out.println(i / ((1 / accuracy) * 60 * 60 * 24));
             }
             for (int j = 1; j < planets.length; j++) {
                 planets[j].initializeAcceleration();
@@ -67,43 +65,33 @@ public class ModelRunner {
     public static void runnerForMultipleProbes(int numberOfDays, double accuracy, List<PlanetObject> planetss, List<Probe> probes) {
         ArrayList<PlanetObject> allObjects = new ArrayList<>(planetss);
         allObjects.addAll(probes);
-        PlanetObject[] planets= allObjects.toArray(new PlanetObject[allObjects.size()]);
-        boolean stopper=false;
+        PlanetObject[] planets = allObjects.toArray(new PlanetObject[allObjects.size()]);
+        boolean stopper = false;
         // check if the boosts
-        for(Probe probe:probes)
-        {
-            if(probe.areBoostsValid(accuracy))
-            {
-            stopper=true;
-                System.out.println("The probe "+probe.getProbeNumber() +" with wrong boost");
+        for (Probe probe : probes) {
+            if (probe.areBoostsValid(accuracy)) {
+                stopper = true;
+                System.out.println("The probe " + probe.getProbeNumber() + " with wrong boost");
             }
         }
 
 
+        if (!stopper) {
+            for (int i = 0; i < (1 / accuracy) * 60 * 60 * 24 * numberOfDays; i += 1) {
 
-        if(!stopper)
-        {
-            for (int i = 0; i < (1 / accuracy) * 60 * 60 * 24 * numberOfDays; i += 1)
-            {
-
-                if (i % ((1 / accuracy) * 60 * 60 * 24) == 0)
-                {
+                if (i % ((1 / accuracy) * 60 * 60 * 24) == 0) {
                     double day = i / ((1 / accuracy) * 60 * 60 * 24);
                     System.out.println("Day " + day);
-                    for (Probe probe : probes)
-                    {
+                    for (Probe probe : probes) {
                         probe.BoosterMECH(day);
                     }
                 }
-                for (int j = 1; j < planets.length; j++)
-                {
+                for (int j = 1; j < planets.length; j++) {
 
                     double[] acc = new double[3];
-                    for (int k = 0; k < planets.length - probes.size(); k++)
-                    {
+                    for (int k = 0; k < planets.length - probes.size(); k++) {
 
-                        if (k != j)
-                        {
+                        if (k != j) {
                             acc = HelperFunctions.addition(acc, planets[j].accelerationBetween(planets[k]));
                         }
                     }
@@ -113,8 +101,6 @@ public class ModelRunner {
         }
 
     }
-
-
 
 
 }
